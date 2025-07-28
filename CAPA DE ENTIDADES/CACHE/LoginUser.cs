d3c0d8b6e0ff4c517a3d60a08bc6b17908f197ed
Angy2025 <angieriverapierre@gmail.com>
2025-07-28 17:39:24 -0400

    1 - Agregue una capa (entidades) que contiene la clase correspondiente a la tabla usuarios, y sus propiedades.
    2 - Cree una carpeta dentro de la capa de presentacion, que contiene las fotos con sus rutas de acceso del gerente y la cajera, respectivamente.

---
+++ CAPA DE ENTIDADES/CACHE/LoginUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_DE_ENTIDADES.CACHE
+{
+    public static class LoginUser
+    {
+        public static int IdUser { get; set; }
+        public static string Nombre { get; set; }
+        public static string Apellido { get; set; }
+        public static string Email { get; set; }
+        public static string Rol { get; set; }
+        public static string Password { get; set; }
+        public static string FotoPath { get; set; }
+    }
+}
