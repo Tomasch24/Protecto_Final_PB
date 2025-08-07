309a81d258e1fc2d609f2581e7ff9df7999a01bc
Angy2025 <angieriverapierre@gmail.com>
2025-08-07 12:05:49 -0400

    Progress Bar

--- Capa_Presentacion/FormBienvenida.cs
+++ Capa_Presentacion/FormBienvenida.cs
@@ -18,18 +18,29 @@ namespace Capa_Presentacion
         {
             InitializeComponent();
         }
+
         private void label2_Click(object sender, EventArgs e)
         {
-
-
+            // Empty click event handler
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private async void timer1_Tick(object sender, EventArgs e)
         {
-            if (this.Opacity < 1) this.Opacity += 0.05;
-            progressBar1.Value += 1;
+            if (this.Opacity < 1)
+            {
+                this.Opacity += 0.05;
+            }
+
+            // Aumenta el valor del ProgressBar solo si no ha llegado a 100
+            if (progressBar1.Value < 100)
+            {
+                progressBar1.Value += 1;
+            }
+
             if (progressBar1.Value == 100)
             {
+                // Simula una operación asíncrona de 1 segundo
+                await Task.Delay(1000);
                 timer1.Stop();
                 timer2.Start();
             }
@@ -38,7 +49,7 @@ namespace Capa_Presentacion
         private void timer2_Tick(object sender, EventArgs e)
         {
             this.Opacity -= 0.05;
-            if (this.Opacity == 0)
+            if (this.Opacity <= 0)
             {
                 timer2.Stop();
                 this.Close();
@@ -48,8 +59,8 @@ namespace Capa_Presentacion
         private void FormBienvenida_Load(object sender, EventArgs e)
         {
             lblusuario.Text = LoginUser.Nombre + " " + LoginUser.Apellido;
-            this.Opacity = 0.0; 
-            timer1.Start(); 
+            this.Opacity = 0.0;
+            timer1.Start();
         }
     }
 }
